11b40ce366b5370180db4399d3b9c0c6491f8b2d
David <davidmunyiri2@gmail.com>
2023-01-29 09:31:53 +0300

    created simple views

--- django_eshop/store/views.py
+++ django_eshop/store/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from matplotlib.style import context
 
-# Create your views here.
+# Store view
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+# Cart view
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+# Check-out view
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
