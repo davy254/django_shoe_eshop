4b3abdf3d17e8f5762b4a6003cb3e97050d65390
David <davidmunyiri2@gmail.com>
2023-01-31 20:05:10 +0300

    written shippinh model

--- django_eshop/store/models.py
+++ django_eshop/store/models.py
@@ -37,3 +37,15 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+# Shipping model
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    town = models.CharField(max_length=200, null=False)
+    county = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.address
