53e4e9feb5645e2737d154c4dff4634aebaf5399
David <davidmunyiri2@gmail.com>
2023-02-03 05:44:48 +0300

    checking for logged in user

--- django_eshop/static/js/cart.js
+++ django_eshop/static/js/cart.js
@@ -1,10 +1,14 @@
 const updateBtns = document.querySelectorAll('.update-cart');
-console.log(updateBtns)
+console.log(updateBtns);
 
 updateBtns.forEach(btn => {
     btn.addEventListener('click', function () {
         let productId = this.dataset.product
         let action = this.dataset.action
         console.log('productId: ', productId, 'Action: ', action)
-    })
+    });
 });
+
+console.log('USER: ', user)
+
+user === 'Anonymous' ?  console.log('User is not authenticated') : console.log('User is authenticated, sending data...')
